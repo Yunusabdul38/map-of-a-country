8edf91cbe2c27e5ef9965c18c42d4bd5cd13c607
Yunus AbdulMajid <Yunusabdulmajidyunus38@gmail.com>
2023-04-23 10:45:56 +0100

    country web app v1.2�

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ This is a web app that display country map, logo and the country name
 -CSS
 -JAVASCRIPT
 -SCSS
+-REST COUNTRY API
 -LEAFLET library
+
+## What I learned 
+I learned alot doing this project and i was able to solved an error that i encounter while using leaflet
+which is the map(html elemtent) which i use to render the map will be initailized once the map is added to the element and it can not be un-initialized  but i was able to fix it by removing and adding back the map(HTML Element) everytime a request is made to rest country api to get the lat and lng of the country then use the coordinate on leaflet to render the map 
+Above all i learned alot a it was fun doing this project
 
 ## Author
 
--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const inputs = document.querySelector(".inputs");
 const countryName = document.querySelector("h3");
 const countryLogo = document.querySelector("img");
 const inputValue = document.querySelector("input");
-//const mapContainer = document.querySelector(".map");
+const preloader = document.querySelector(".preloader");
 const btn = document.querySelector(".btn");
 const container = document.querySelector(".container");
 const html = ` <section class="map" id="map"></section>`;
 container.insertAdjacentHTML("beforeend", html);
+
+// map style variable from openStreet map
 const map_style = [
   [
     "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -40,55 +42,67 @@ const map_style = [
     },
   ],
 ];
-let styleNumber
 
-const request = fetch(`https://restcountries.com/v3.1/name/${"nigeria"}`).then(res=>console.log(res));
-
+// async Ajax call
 async function country(name, style=0) {
   try {
-    
+    // fetch request for name of a country using rest country api
     const request = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+    //remove map countainer when ever we try(call this function) to re initialized map element
     container.lastChild.remove();
+
+    //adding map html element back
     container.insertAdjacentHTML("beforeend", html);
-    console.log(request)
+    // if request failed 
     if (!request.ok) {
       throw new Error(
         `(${request.status}) Unable to display map with the country name ${name} check the name and try again`
       );
     }
+    Preloader()
     const [response] = await request.json();
     const { latlng } = await response.capitalInfo;
-    const [lat, lng] = await latlng;
-    //map.remove()
-    // countryLogo.src = response.flags.svg;
-    // countryName.textContent = response.name.common;
-    // response.flags.alt
-    //   ? (countryLogo.alt = response.flags.alt)
-    //   : (countryLogo.alt = `flag of ${name}`);
-    map = await L.map("map").setView([lat, lng], 13);
+    map = await L.map("map").setView(latlng, 13);
+    //map tilelayer style from map_style variable
     let styleValue = map_style[style];
+    //adding map tilelayer
     const tile=await L.tileLayer(styleValue[0], styleValue[1]).addTo(map);
+    // remove preloader after tilelayer finish loading
+    tile.addEventListener("load",function(){
+      preloader.style.display="none"
+      container.style.filter="blur(0px)"     
+    })
+    // adding the country name, logo and atl
     countryLogo.src = response.flags.svg;
     countryName.textContent = response.name.common;
     response.flags.alt
       ? (countryLogo.alt = response.flags.alt)
       : (countryLogo.alt = `flag of ${name}`);
+    //adding marker to the coordinate of the country capital
+    L.marker(latlng).addTo(map).bindPopup(`Capital of ${name}`).openPopup();
 
-    //console.log(map,mapContainer);
-    L.marker([lat, lng]).addTo(map).bindPopup(`Capital of ${name}`).openPopup();
+    // catching error
   } catch (err) {
     console.log(err);
-    //console.log(mapContainer)
     alert(err);
   }
 }
+inputValue.focus()
 inputValue.value = "";
+//default country name Onload
 let countryValue='nigeria'
+
+//button event
 btn.addEventListener("click", () => {
   countryValue = inputValue.value;
   if (!countryValue) return;
   country(countryValue);
 });
+function Preloader(){
+  container.style.filter="blur(8px)"
+  preloader.style.display="block"
+}
+// input event 
 inputValue.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     countryValue = inputValue.value;
@@ -96,6 +110,7 @@ inputValue.addEventListener("keyup", (e) => {
     country(countryValue);
   }
 });
+//map tilelayer styles events onClick
 document.querySelector(".map_style").addEventListener("click", (e) => {
   if(e.target.classList.contains("box")){
     const id = +e.target.dataset.id
@@ -104,4 +119,3 @@ document.querySelector(".map_style").addEventListener("click", (e) => {
   }
 });
 country(countryValue);
-// mint();
